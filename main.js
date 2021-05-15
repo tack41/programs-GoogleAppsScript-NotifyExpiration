f8302b13585f60e4d2e0dee667a65fb4e6e17e09
Takashi Tsuduki <tack41@gmail.com>
2021-05-15 17:54:52 +0900

    不要なvarをconst,letに修正、log出力を追加

--- main.js
+++ main.js
@@ -9,53 +9,68 @@
 function NotifyExpiration(targetSpreadFileID, targetSheetName, targetRow) {
 
   //対象シートを取得
-  var spreadApp = SpreadsheetApp.openById(targetSpreadFileID);
-  var targetSheet = spreadApp.getSheetByName(targetSheetName);
+  const spreadApp = SpreadsheetApp.openById(targetSpreadFileID);
+  const targetSheet = spreadApp.getSheetByName(targetSheetName);
 
   //今の時刻を取得
-  var now = new Date();
+  const now = new Date();
  
   //対象行のデータを取得
-  var expirationName = targetSheet.getRange(targetRow, 1, 1, 1).getValue();
-  var expirationTime = new Date(targetSheet.getRange(targetRow, 2, 1, 1).getValue());
-  var alertHourFroms = targetSheet.getRange(targetRow, 3, 1, 1).getValue().toString().split(',');
+  const expirationName = targetSheet.getRange(targetRow, 1, 1, 1).getValue();
+  const expirationTime = new Date(targetSheet.getRange(targetRow, 2, 1, 1).getValue());
+  const alertHourFroms = targetSheet.getRange(targetRow, 3, 1, 1).getValue().toString().split(',');
+  const comment = targetSheet.getRange(targetRow, 4, 1, 1).getValue().toString().split(',');
   
+  // 1列目の管理対象名が未指定の場合は実行しない
+  if(expirationName.trim().length == 0){
+    console.log(`line ${targetRow} didn't processed because name(column1) isn't specified.`);
+    return;
+  }
+
   //今日から有効期限までの残り時間(h)を計算
-  var hoursLeft = Math.round((expirationTime.getTime()-now.getTime())/(1000 * 60 * 60));
+  const hoursLeft = Math.round((expirationTime.getTime()-now.getTime())/(1000 * 60 * 60));
   
   //有効期限の通知前時間が今の場合にメール通知する(1時間おきに起動される想定)
+  let sent = false
   for(const alertHourFrom of alertHourFroms){    
     if(hoursLeft == alertHourFrom){
-      sendMessage(expirationName + 'の期限があと' + Math.round(hoursLeft) + '時間です')
+      sendMessage(`${expirationName}の期限があと${Math.round(hoursLeft)}時間です\n${comment}`);
+      console.log(`Message for '${expirationName}' sent because hours left: ${hoursLeft}`);
+      sent = true;
+      break;
     }
+  }
+  if(!sent){
+    console.log(`Message for '${expirationName}' didn't sent because hours left: ${hoursLeft}`);
   }
 }
 
 function callerForAllRow() {
 
-  var targetSpreadFileID = PropertiesService.getScriptProperties().getProperty('TARGET_SPREADSHEET_ID');
-  var targetSheetName = PropertiesService.getScriptProperties().getProperty('TARGET_SHEET_NAME');
+  const targetSpreadFileID = PropertiesService.getScriptProperties().getProperty('TARGET_SPREADSHEET_ID');
+  const targetSheetName = PropertiesService.getScriptProperties().getProperty('TARGET_SHEET_NAME');
   
   //対象シートを取得
-  var spreadApp = SpreadsheetApp.openById(targetSpreadFileID);
-  var targetSheet = spreadApp.getSheetByName(targetSheetName);
+  const spreadApp = SpreadsheetApp.openById(targetSpreadFileID);
+  const targetSheet = spreadApp.getSheetByName(targetSheetName);
 
   //最終行を取得
-  var lastRow = targetSheet.getLastRow();
+  const lastRow = targetSheet.getLastRow();
+  console.log(`Last row: ${lastRow}`)
   
-  var expired = 0;
   //1行目(タイトル行)を除く2行目から最終行に対してチェックを実行
-  for(var i=2; i<=lastRow; i++){
-    NotifyExpiration(targetSpreadFileID, targetSheetName, i)
+  for(let i=2; i<=lastRow; i++){
+    NotifyExpiration(targetSpreadFileID, targetSheetName, i);
+    console.log(`NotifyExpiration done for row: ${i}`);
   }
 }
 
 function sendMessage(message) {
   //Line Notifyのトークン
-  var lineTokenFamily = PropertiesService.getScriptProperties().getProperty('lineTokenFamily')
-  var lineTokenDebug = PropertiesService.getScriptProperties().getProperty('lineTokenDebug')
+  const lineTokenFamily = PropertiesService.getScriptProperties().getProperty('lineTokenFamily')
+  const lineTokenDebug = PropertiesService.getScriptProperties().getProperty('lineTokenDebug')
 
-  var options =
+  const options =
    {
      "method"  : "post",
      "payload" : "message=" + message,
@@ -63,4 +78,5 @@ function sendMessage(message) {
    };
 
    UrlFetchApp.fetch("https://notify-api.line.me/api/notify",options);
+   console.log(`Message sent.`);
 }
